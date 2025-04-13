bdb009ea49a6c36c278256093934363104a957e5
Zosha Chung <czosha@gmail.com>
2025-04-13 18:59:31 -0400

    Apply responsive styling to tour components

---
+++ src/components/LoadSpinner.jsx
@@ -0,0 +1,10 @@
+const LoadingSpinner = () => {
+    return (
+      <div className="loading-container">
+        <div className="loading-spinner"></div>
+        <p>Loading amazing tours...</p>
+      </div>
+    );
+  };
+  
+  export default LoadingSpinner; 
